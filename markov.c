9c9d83b89ba0a69ad6ff3a5d13143194fd876a9c
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-05-11 23:56:25 -0500

    HAAAHAHAHAHA

--- markov.c
+++ markov.c
@@ -1,112 +1,116 @@
+#include <errno.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "m_pd.h"
+
+#define MAX_LINE_SIZE 1024
 
 static t_class *markov_class;
 
+typedef struct _probability_matrix {
+  int state_len;
+  int n_elements;
+  int n_states;  // n_elements ** state_len
+
+  char *elements;
+  char **states;          // (index, state)
+  float **probabilities;  // (index, probability)
+} probability_matrix;
+
 typedef struct _markov {
   t_object x_obj;
-  t_int init_count, current_count;
-  t_int mod_A, mod_B;
-  t_inlet *in_A, *in_B;
-  t_outlet *out_A, *out_B, *out_sync, *out_count;
+  const char *f_path;
+
+  probability_matrix m;
 } t_markov;
 
+int csv_to_pm(probability_matrix m, const char *f_path) {
+  FILE *file = fopen(f_path, "r");
+  if (file == NULL) {
+    post("Error opening file %s. %s", f_path, strerror(errno));
+    return 1;
+  }
+
+  // Allocate `probability_matrix`
+  m.elements = (char *)malloc(m.n_elements * sizeof(char));
+  m.states = (char **)malloc(m.n_states * sizeof(char *));
+  m.probabilities = (float **)malloc(m.n_states * sizeof(float *));
+  for (int i = 0; i < m.n_states; i++)
+    m.probabilities[i] = (float *)malloc(m.n_elements * sizeof(float));
+
+  char line[MAX_LINE_SIZE];
+  int line_count = 0;
+
+  while (fgets(line, sizeof(line), file)) {
+    char *token = strtok(line, ",");
+    int col_count = 0;
+
+    while (token != NULL) {
+      if (col_count == 0) {  // Skip first (empty/dummy) entry in first row
+        token = strtok(NULL, ",");
+        ++col_count;
+        continue;
+      }
+
+      if (line_count == 0) {  // Elements
+        if (col_count > 0) m.elements[col_count - 1] = *token;
+      } else {
+        if (col_count == 0)  // State
+          m.states[line_count - 1] = strdup(token);
+        else  // Probabilities
+          m.probabilities[line_count - 1][col_count - 1] = atof(token);
+      }
+
+      token = strtok(NULL, ",");
+      col_count++;
+    }
+    line_count++;
+  }
+
+  fclose(file);
+  return 0;
+}
+
 void print(t_markov *x) {
-  post("[markov ] init_count=%d, current_count=%d", x->init_count,
-       x->current_count);
-  post("[markov ] mod_A=%d, mod_B=%d", x->mod_A, x->mod_B);
-}
+  post("[markov] f_path=%s", x->f_path);
+  post("[markov] state_len=%d, n_elements=%d, n_states=%d", x->m.state_len,
+       x->m.n_elements, x->m.n_states);
 
-void set_counts(t_markov *x, t_floatarg init, t_floatarg curr) {
-  x->init_count = init;
-  x->current_count = curr;
-}
-
-void reset_counts(t_markov *x) { set_counts(x, 0, 0); }
-
-void set_mods(t_markov *x, t_floatarg a, t_floatarg b) {
-  x->mod_A = a <= 0 ? 1 : a;
-  x->mod_B = b <= 0 ? 1 : b;
-}
-
-void on_bang(t_markov *x) {
-  print(x);
-
-  t_int mod_A = x->mod_A;
-  t_int mod_B = x->mod_B;
-  t_int mod_sync = mod_A * mod_B;
-  t_int n = x->current_count;
-
-  if (n % mod_sync == 0) {
-    outlet_bang(x->out_sync);
-    x->current_count = 0;
+  post("[markov] matrix:");
+  for (int i = 0; i < x->m.n_states; i++) {
+    post("[markov] %s", x->m.states[i]);
+    for (int j = 0; j < x->m.n_elements; j++)
+      post("[markov] %c: %f", x->m.elements[j], x->m.probabilities[i][j]);
   }
-  if (n % mod_A == 0) outlet_bang(x->out_A);
-  if (n % mod_B == 0) outlet_bang(x->out_B);
-
-  ++x->current_count;
 }
 
-void onset_A(t_markov *x, t_floatarg a) { set_mods(x, a, x->mod_B); }
+void on_bang(t_markov *x) { print(x); }
 
-void onset_B(t_markov *x, t_floatarg b) { set_mods(x, x->mod_A, b); }
-
-void on_reset(t_markov *x) { reset_counts(x); }
-
-void on_list(t_markov *x, t_symbol *_, t_int argc, t_atom *argv) {
-  (void)_;
-
-  if (argc == 2) {
-    set_mods(x, atom_getfloat(argv), atom_getfloat(argv + 1));
-    return;
-  }
-
-  pd_error(&x->x_obj, "[markov ] Expected two arguments in list");
-}
-
-void *init(t_floatarg a, t_floatarg b) {
+void *init(const t_symbol *t_f_path_sym, const t_floatarg t_state_len,
+           const t_floatarg t_n_states) {
   t_markov *x = (t_markov *)pd_new(markov_class);
 
-  reset_counts(x);
-  set_mods(x, a, b);
+  x->f_path = t_f_path_sym->s_name;
+  x->m.state_len = t_state_len;
+  x->m.n_states = t_n_states;
+  x->m.n_elements = pow(t_state_len, t_n_states);
 
-  x->in_A = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("ratio_A"));
-  x->in_B = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("ratio_B"));
-
-  x->out_A = outlet_new(&x->x_obj, &s_bang);
-  x->out_B = outlet_new(&x->x_obj, &s_bang);
-  x->out_sync = outlet_new(&x->x_obj, &s_bang);
-  x->out_count = outlet_new(&x->x_obj, &s_float);
-
-  print(x);
+  csv_to_pm(x->m, x->f_path);
 
   return x;
 }
 
-void destroy(t_markov *x) {
-  inlet_free(x->in_A);
-  inlet_free(x->in_B);
-
-  outlet_free(x->out_A);
-  outlet_free(x->out_B);
-  outlet_free(x->out_sync);
-  outlet_free(x->out_count);
-}
+void destroy(t_markov *x) { (void)x; }
 
 void markov_setup() {
-  markov_class = class_new(gensym("markov"),   // Object name
-                           (t_newmethod)init,  // Initializer
-                           (t_method)destroy,  // Destructor
-                           sizeof(t_markov),   // Object size
-                           CLASS_DEFAULT,      // Rectangle object
-                           //  Creation arguments: float (A), float (B)
-                           A_DEFFLOAT, A_DEFFLOAT, 0);
+  markov_class = class_new(gensym("markov"), (t_newmethod)init,
+                           (t_method)destroy, sizeof(t_markov), CLASS_DEFAULT,
+                           A_DEFSYMBOL, A_DEFFLOAT, A_DEFFLOAT, A_DEFFLOAT, 0);
 
   class_addbang(markov_class, (t_method)on_bang);
-  class_addmethod(markov_class, (t_method)onset_A, gensym("ratio_A"),
-                  A_DEFFLOAT, 0);
-  class_addmethod(markov_class, (t_method)onset_B, gensym("ratio_B"),
-                  A_DEFFLOAT, 0);
-  class_addmethod(markov_class, (t_method)on_reset, gensym("reset"), 0);
-  class_addlist(markov_class, (t_method)on_list);
+
   class_sethelpsymbol(markov_class, gensym("markov"));
 }
